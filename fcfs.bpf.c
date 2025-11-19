26abfc18f9b287b14ab7fcb2895545219aa90823
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-19 16:14:33 +0800

    fix: used linux version to map interfaces

--- fcfs.bpf.c
+++ fcfs.bpf.c
@@ -8,12 +8,12 @@
 // Define a shared Dispatch Queue (DSQ) ID
 #define SHARED_DSQ_ID 0
 
-#define BPF_STRUCT_OPS(name, args...)	\
-    SEC("struct_ops/"#name)	BPF_PROG(name, ##args)
+#define BPF_STRUCT_OPS(name, args...)                                          \
+  SEC("struct_ops/" #name) BPF_PROG(name, ##args)
 
-#define BPF_STRUCT_OPS_SLEEPABLE(name, args...)	\
-    SEC("struct_ops.s/"#name)				    \
-    BPF_PROG(name, ##args)
+#define BPF_STRUCT_OPS_SLEEPABLE(name, args...)                                \
+  SEC("struct_ops.s/" #name)                                                   \
+  BPF_PROG(name, ##args)
 
 // We use the new names from 6.13 to make it more readable
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 13, 0)
@@ -24,41 +24,33 @@
 #define scx_bpf_dsq_move_vtime scx_bpf_dispatch_vtime_from_dsq
 #endif
 
-
-
-
-
 // Initialize the scheduler by creating a shared dispatch queue (DSQ)
 s32 BPF_STRUCT_OPS_SLEEPABLE(sched_init) {
-    return scx_bpf_create_dsq(SHARED_DSQ_ID, -1);
+  return scx_bpf_create_dsq(SHARED_DSQ_ID, -1);
 }
 
 // Enqueue a task to the shared DSQ, dispatching it with a time slice
 int BPF_STRUCT_OPS(sched_enqueue, struct task_struct *p, u64 enq_flags) {
-    // Give the task an infinite time slice, so it only goes from the CPU
-    // on its own accord
-    scx_bpf_dsq_insert(p, SHARED_DSQ_ID, -1, enq_flags);
-    return 0;
+  // Give the task an infinite time slice, so it only goes from the CPU
+  // on its own accord
+  scx_bpf_dsq_insert(p, SHARED_DSQ_ID, -1, enq_flags);
+  return 0;
 }
 
 // Dispatch a task from the shared DSQ to a CPU
 int BPF_STRUCT_OPS(sched_dispatch, s32 cpu, struct task_struct *prev) {
-	scx_bpf_dsq_move_to_local(SHARED_DSQ_ID);
-    return 0;
+  scx_bpf_dsq_move_to_local(SHARED_DSQ_ID);
+  return 0;
 }
-
-
-
 
 // Define the main scheduler operations structure (sched_ops)
 SEC(".struct_ops.link")
-struct sched_ext_ops sched_ops = {
-    .enqueue   = (void *)sched_enqueue,
-    .dispatch  = (void *)sched_dispatch,
-    .init      = (void *)sched_init,
-    .flags     = SCX_OPS_ENQ_LAST | SCX_OPS_KEEP_BUILTIN_IDLE,
-    .name      = "fcfs_scheduler"
-};
+struct sched_ext_ops sched_ops = {.enqueue = (void *)sched_enqueue,
+                                  .dispatch = (void *)sched_dispatch,
+                                  .init = (void *)sched_init,
+                                  .flags = SCX_OPS_ENQ_LAST |
+                                           SCX_OPS_KEEP_BUILTIN_IDLE,
+                                  .name = "fcfs_scheduler"};
 
 // License for the BPF program
 char _license[] SEC("license") = "GPL";
